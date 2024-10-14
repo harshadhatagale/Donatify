aebc2d3e67aa47fe7267346f615ec78ae6794af2
harshadhatagale <harshadhatagale6@gmail.com>
2024-10-14 16:09:18 +0530

    init

--- src/components/ui/Nav.jsx
+++ src/components/ui/Nav.jsx
@@ -16,24 +16,27 @@ const roboto = Roboto({
 export default function Nav() {
 
   const [isNavOpen, setNavOpen]= useState(false)
-  const togggleNav=()=>{
+  const openNav=()=>{
     setNavOpen(!isNavOpen)
+  }
+  const closeNav=()=>{
+    setNavOpen(false)
   }
   return (
     <div className={`${isNavOpen ? "active-nav" : ""} lg:px-5 px-4 shadow-md fixed top-0 bg-purple-700 h-16 w-full flex justify-between items-center`}>
       <div className='flex h-16 justify-between items-center lg:w-auto w-full '>
-        <Link href={"/"} onClick={togggleNav} className='flex space-x-2 justify-center items-center text-2xl text-white font-bold'>
+        <Link href={"/"} onClick={closeNav} className='flex space-x-2 justify-center items-center text-2xl text-white font-bold'>
           <FcLike /> <span className={`${poppins.className}`}>Donatify</span>
         </Link>
-        <RiMenu3Fill onClick={togggleNav} size={24} color='white' className='lg:hidden flex justify-center items-center' />
+        <RiMenu3Fill onClick={openNav} size={24} color='white' className='lg:hidden flex justify-center items-center' />
       </div>
       <div className={`${roboto.className} ${isNavOpen ? "active-links" : ""} lg:block hidden  mx-5`}>
         <ul className='lg:flex lg:h-16 justify-center items-center space-x-3 text-md font-semibold text-white'>
-          <li onClick={togggleNav}><Link href={"/"}>Home</Link></li>
-          <li onClick={togggleNav}><Link href={"/about"}>About us</Link></li>
-          <li onClick={togggleNav}><Link href={"/"}>Donations</Link></li>
-          <li onClick={togggleNav}><Link href={"/how_it_works"}>How it works</Link></li>
-          <li onClick={togggleNav}><Link href={"/contact"}>Contact Us</Link></li>
+          <li onClick={closeNav}><Link href={"/"}>Home</Link></li>
+          <li onClick={closeNav}><Link href={"/about"}>About us</Link></li>
+          <li onClick={closeNav}><Link href={"/"}>Donations</Link></li>
+          <li onClick={closeNav}><Link href={"/how_it_works"}>How it works</Link></li>
+          <li onClick={closeNav}><Link href={"/contact"}>Contact Us</Link></li>
         </ul>
       </div>
     </div>
